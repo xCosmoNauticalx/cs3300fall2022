8e9c5f3fce7e98e272fc58c498f15c9e40ffa660
xcosmonauticalx <kelsey.garcia739@gmail.com>
2022-11-25 18:52:16 +0000

    Styled new project page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+  root "projects#new"
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
