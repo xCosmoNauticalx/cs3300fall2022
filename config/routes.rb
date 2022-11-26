554d7d853a7eaf43fd35f44c7a881617852646be
xcosmonauticalx <kelsey.garcia739@gmail.com>
2022-11-26 20:04:31 +0000

    finished iteration 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "projects#new"
+  root "projects#index"
+
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
